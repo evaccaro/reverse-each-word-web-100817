40831519e8590e0d5df5b0a65a003b6be9d60a99
Elisa Vaccaro <erv230@nyu.edu>
2017-10-03 03:19:29 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,9 @@
 def reverse_each_word(str)
   answer = ""
+  to_array.join(" ")
   to_array = str.split(" ")
   to_array.each do |word|
-    answer += "#{word.reverse} ".slice(0, -1)
+    answer += word.reverse
   end
   answer
 end
