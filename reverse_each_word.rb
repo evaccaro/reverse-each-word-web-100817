1fdfb37e80f1949ba196b5c3e240295c9920a737
Elisa Vaccaro <erv230@nyu.edu>
2017-10-03 03:30:53 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,11 @@ def reverse_each_word(str)
   end
   answer.chomp(" ")
 end
+
+def reverse_each_word(str)
+  to_array = str.split(" ")
+  to_array.collect do |word|
+    "#{word.reverse} "
+  end
+  answer.chomp(" ")
+end
